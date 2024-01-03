8115c1dc47fb98eb36a0c2a9d0b79e54586d6046
Rachit Goyal <rachit.promo@gmail.com>
2024-01-03 10:06:36 +0530

    working version

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import numpy as np
 from sdv.metadata import SingleTableMetadata
 from sdv.single_table import CTGANSynthesizer,GaussianCopulaSynthesizer,TVAESynthesizer
 from sdv.lite import SingleTablePreset
+from datetime import date
 
-#This needs to change
+
 def Gen_data(model,rows):
     try:
         if model=='FastML':
@@ -24,7 +25,7 @@ def Gen_data(model,rows):
             row=int(rows)
             
             syn_gen_data=syn.sample(num_rows=row)
-            return st.write(syn_gen_data)
+            return syn_gen_data
     except ValueError:
         return "Invalid option chosen."
     
@@ -44,6 +45,12 @@ def db_tables(db):
     st.write(df_mer3.head(10))
     return df_mer3
 
+def show_tables(db):
+    conn = sqlite3.connect(db)
+    sql_query = """SELECT name FROM sqlite_master WHERE type='table';"""
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    return (st.write(cursor.fetchall()))
 
 def impute_all():
     st.write('Starting Imputation..')
@@ -72,69 +79,82 @@ def meta_validation(dat):
     st.write('Validation completed')
     return metadata
 
-def train_fml(dat,metadata,out_pkl):
+def train_fml(dat,metadata):
     st.write('Training starting...')
     synth = SingleTablePreset(metadata, name='FAST_ML')
     synth.fit(df2)
-    synth.save(filepath=out_pkl)
-    st.write(f'Training completed, model is saved: {out_pkl}')
+    x='./custom_model/fml_'+date.today().strftime("%b-%d-%Y")+".pkl"
+    synth.save(filepath=x)
+    st.write(f'Training completed, model is saved: {x}')
 
-def train_gcs(dat,metadata,out_pkl):
+def train_gcs(dat,metadata):
     st.write('Training starting...')
     synth = GaussianCopulaSynthesizer(metadata)
     synth.fit(df2)
-    synth.save(filepath=out_pkl)
-    st.write(f'Training completed, model is saved: {out_pkl}')
+    x='./custom_model/gcs_'+date.today().strftime("%b-%d-%Y")+".pkl"
+    synth.save(filepath=x)
+    st.write(f'Training completed, model is saved: {x}')
 
-def train_ctgan(dat,metadata,out_pkl):
+def train_ctgan(dat,metadata):
     st.write('Training starting...')
     synth = CTGANSynthesizer(metadata,epochs=100,verbose=True)
     synth.fit(df2)
-    synth.save(filepath=out_pkl)
-    st.write(f'Training completed, model is saved: {out_pkl}')
+    x='./custom_model/ctgan_'+date.today().strftime("%b-%d-%Y")+".pkl"
+    synth.save(filepath=x)
+    st.write(f'Training completed, model is saved: {x}')
 
-def train_tvae(dat,metadata,out_pkl):
+def train_tvae(dat,metadata):
     st.write('Training starting...')
     synth = TVAESynthesizer(metadata,epochs=100,verbose=True)
     synth.fit(df2)
-    synth.save(filepath=out_pkl)
-    st.write(f'Training completed, model is saved: {out_pkl}')
+    x='./custom_model/tvae_'+date.today().strftime("%b-%d-%Y")+".pkl"
+    synth.save(filepath=x)
+    st.write(f'Training completed, model is saved: {x}')
 
 
 st.title('Welcome to V1 version of data synthesizer :smile:')
-task=st.sidebar.selectbox('Select your choice:',['Train a model','Generate data'])            
+task=st.sidebar.selectbox('Select your choice:',['Train a model','Generate data','Model Quality report'])            
 
 st.divider()
-
 if task=='Train a model':
     st.write('Connect to data source: Database or Flatfiles')
     dat_src=st.radio('Choose data source',['Connect to a db','Upload files'])
     st.divider()
     if dat_src=='Connect to a db':
-        st.write('Connecting to db...')
-        df_mer3=db_tables('Source_data.db')
-        st.write('Data has been loaded and combined')
-        st.divider()
-        st.write('Missing value status: ',df_mer3.isna().sum())
-        st.write('Imputing all')
-        df2=impute_all()
-        st.write('Imputation completed')
-        st.divider()
-        metadata=meta_validation(df2)
-        m_choice=['FastML','Gaussian Copula Synthesizer','CT-GAN','TVAE']
-        mt_selection=st.selectbox('Choose a model to generate data: ',m_choice,index=0)
-        epoch=st.number_input('Mention number of epochs',min_value=1,max_value=1000)
-        if mt_selection=='FastML':
-            train_fml(df2,metadata,'custom_model/my_synth_fml_sys_v1.pkl')
-        elif mt_selection=='Gaussian Copula Synthesizer':
-            train_gcs(df2,metadata,'custom_model/my_synth_gcs_sys_v1.pkl')
-        elif mt_selection=='CT-GAN':
-            train_ctgan(df2,metadata,'custom_model/my_synth_ctgan_sys_v1.pkl')
-        elif mt_selection=='TVAE':
-            train_tvae(df2,metadata,'custom_model/my_synth_tvae_sys_v1.pkl')
-        
-    elif dat_src=='Upload files':
-        st.file_uploader('Upload file')
+        show_table_ind=st.checkbox('Show data in DB')
+        if show_table_ind:
+            st.write('Connecting to db...')
+            show_tables('Source_data.db')
+            load_data_ind=st.checkbox('Load data')
+            if load_data_ind:
+                df_mer3=db_tables('Source_data.db')
+                st.write('Data has been loaded')
+                missing_status_ind=st.checkbox('Show missing values in data')
+                if missing_status_ind:
+                    st.write(df_mer3.isna().sum())
+                    impute_all_ind=st.checkbox('Impute all')
+                    if impute_all_ind:
+                        df2=impute_all()
+                        meta_validation_ind=st.checkbox('Validate metadata')
+                        if meta_validation_ind:
+                            metadata=meta_validation(df2)
+                            st.write('Train model')
+                            col1,col2=st.columns(2)
+                            with col1:
+                                model_train=['FastML','Gaussian Copula Synthesizer','CT-GAN','TVAE']
+                                mchoice=st.selectbox('Select model',model_train)
+                            with col2:
+                                epoch_input=st.number_input('Select Epochs:',1,1000,step=2)
+                            train_ind=st.checkbox('Train model')
+                            if train_ind:
+                                if mchoice=='FastML':
+                                    train_fml(df2,metadata)
+                                elif mchoice=='Gaussian Copula Synthesizer':
+                                    train_gcs(df2,metadata)
+                                elif mchoice=='CT-GAN':
+                                    train_ctgan(df2,metadata)
+                                elif mchoice=='TVAE':
+                                    train_tvae((df2,metadata))
 elif task=='Generate data':
     models=['FastML','Gaussian Copula Synthesizer','CT-GAN','TVAE','Custom model']
     m_selection=st.selectbox('Choose a model to generate data: ',models,index=0)
@@ -145,25 +165,36 @@ elif task=='Generate data':
     if gen_button:
         st.write('Generating data....Please wait..')
         df_synth=Gen_data(m_selection,rows)
-        st.write('Disintegrating tables..')
+        st.write(df_synth)
         
+        l_add=['ADDRESS_ID', 'LATITUDE', 'LONGITUDE', 'STREET_ADDRESS', 'CITY','STATE', 'COUNTY']
+        l_customer=['INDIVIDUAL_ID', 'ADDRESS_ID', 'CURR_ANN_AMT', 'DAYS_TENURE','CUST_ORIG_DATE', 'AGE_IN_YEARS', 'DATE_OF_BIRTH','SOCIAL_SECURITY_NUMBER'] 
+        l_demographic=['INDIVIDUAL_ID', 'INCOME', 'HAS_CHILDREN', 'LENGTH_OF_RESIDENCE','MARITAL_STATUS', 'HOME_MARKET_VALUE', 'HOME_OWNER', 'COLLEGE_DEGREE','GOOD_CREDIT']
+        l_termination=['INDIVIDUAL_ID', 'ACCT_SUSPD_DATE']
+        df_temp=df_synth[l_add]
+        df_sys_add=df_temp.drop_duplicates(subset=['ADDRESS_ID'],keep='last')
 
-        df_temp=df_synth['ADDRESS_ID', 'LATITUDE', 'LONGITUDE', 'STREET_ADDRESS', 'CITY','STATE', 'COUNTY']
-        df_address=df_temp.drop_duplicates(subset=['ADDRESS_ID'],keep='last')
-        df_temp=df_synth['INDIVIDUAL_ID', 'ADDRESS_ID', 'CURR_ANN_AMT', 'DAYS_TENURE','CUST_ORIG_DATE', 'AGE_IN_YEARS', 'DATE_OF_BIRTH','SOCIAL_SECURITY_NUMBER']
-        df_customer=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
-        df_temp=df_synth['INDIVIDUAL_ID', 'INCOME', 'HAS_CHILDREN', 'LENGTH_OF_RESIDENCE','MARITAL_STATUS', 'HOME_MARKET_VALUE', 'HOME_OWNER', 'COLLEGE_DEGREE','GOOD_CREDIT']
-        df_demographic=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
-        df_temp=df_synth['INDIVIDUAL_ID', 'ACCT_SUSPD_DATE']
-        df_termination=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
+        df_temp=df_synth[l_customer]
+        df_sys_cust=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
+
+        df_temp=df_synth[l_demographic]
+        df_sys_demo=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
+
+        df_temp=df_synth[l_termination]
+        df_sys_term=df_temp.drop_duplicates(subset=['INDIVIDUAL_ID'],keep='last')
+
         one,two,three,four=st.columns(4)
         with one:
-            st.write(df_address)
+           st.text('Address file')
+           st.write(df_sys_add)
         with two:
-            st.write(df_customer)
+            st.text('Customer file')
+            st.write(df_sys_cust)
         with three:
-            st.write(df_demographic)
+            st.text('Demographics file')
+            st.write(df_sys_demo)
         with four:
-            st.write(df_termination)
-        
-
+            st.text('Termination file')
+            st.write(df_sys_term)
+elif task=='Model Quality report':
+    st.write('Area under construction')
