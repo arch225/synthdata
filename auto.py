b96d78e4b9c985dc612f82dde42dad7b0836ceb0
Rachit Goyal <rachit.promo@gmail.com>
2024-02-01 20:23:16 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import SessionState
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Sample DataFrame
+data = {'value': [10, 15, 20, 25, 30, 35],
+        'indicator': ['A', 'B', 'A', 'B', 'A', 'B']}
+
+df = pd.DataFrame(data)
+
+# Create subplots with histograms based on the 'indicator' column
+g = sns.FacetGrid(df, col='indicator', height=4, sharey=False)
+g.map(plt.hist, 'value', bins=10, color='skyblue')
+
+# Set plot labels and titles
+g.set_axis_labels('Value', 'Frequency')
+g.set_titles('Histogram for Indicator {col_name}')
+
+# Show the plots
+plt.show()
 
 def main():
     st.subheader("new")
